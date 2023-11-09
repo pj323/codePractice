fd10bc95778931564970e9c3c028bd610184eea4
Preetham John <preethamjohn@Preethams-MacBook-Air.local>
2023-11-09 12:36:06 -0800

    Initial Commit

---
+++ Calculator.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+// Function declarations
+float add(float a, float b);
+float subtract(float a, float b);
+float multiply(float a, float b);
+float divide(float a, float b);
+
+int main() {
+    float num1, num2;
+    char operation;
+
+    // Input
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter an operation (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Perform the operation and display the result
+    switch (operation) {
+        case '+':
+            cout << "Result: " << add(num1, num2) << endl;
+            break;
+        case '-':
+            cout << "Result: " << subtract(num1, num2) << endl;
+            break;
+        case '*':
+            cout << "Result: " << multiply(num1, num2) << endl;
+            break;
+        case '/':
+            // Check for division by zero
+            if (num2 != 0) {
+                cout << "Result: " << divide(num1, num2) << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Error: Invalid operation" << endl;
+            break;
+    }
+
+    return 0;
+}
+
+// Function definitions
+float add(float a, float b) {
+    return a + b;
+}
+
+float subtract(float a, float b) {
+    return a - b;
+}
+
+float multiply(float a, float b) {
+    return a * b;
+}
+
+float divide(float a, float b) {
+    return a / b;
+}
