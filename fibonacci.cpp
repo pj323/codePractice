78183a638ab1654ac2dda57f43ec4aa76dd6be6c
Preetham John <preethamjohn@Preethams-MacBook-Air.local>
2023-11-11 10:36:14 -0800

    Update Commit

---
+++ fibonacci.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+int main() {
+    int n;
+
+    std::cout << "Enter the number of terms for Fibonacci sequence: ";
+    std::cin >> n;
+
+    std::cout << "Fibonacci Sequence:\n";
+    for (int i = 0; i < n; ++i) {
+        std::cout << fibonacci(i) << " ";
+    }
+
+    return 0;
+}
